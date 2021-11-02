3f99c85aac61296cfa84e086e43318956993e5c2
ForsakendMJr <64882709+ForsakendMJr@users.noreply.github.com>
2021-11-02 15:54:35 +0000

    Update V20.5

--- login-page.js
+++ login-page.js
@@ -9,25 +9,25 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "Chamitha" && password === "1984-1") {
         alert("You have successfully logged in.");
-        location.replace('https://forsakendteam.github.io/1984.github.io/');
+        location.replace('https://forsakendteam.github.io/1984.github.io/hub.html');
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "Samir" && password === "1984-2") {
         alert("You have successfully logged in.");
-        location.replace('https://forsakendteam.github.io/1984.github.io/');
+        location.replace('https://forsakendteam.github.io/1984.github.io/hub.html');
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "Theo" && password === "1984-3") {
         alert("You have successfully logged in.");
-        location.replace('https://forsakendteam.github.io/1984.github.io/');
+        location.replace('https://forsakendteam.github.io/1984.github.io/hub.html');
     } else {
         loginErrorMsg.style.opacity = 1;
     }
     if (username === "Jack" && password === "1984-4") {
         alert("You have successfully logged in.");
-        location.replace('https://forsakendteam.github.io/1984.github.io/');
+        location.replace('https://forsakendteam.github.io/1984.github.io/hub.html');
     } else {
         loginErrorMsg.style.opacity = 1;
     }
