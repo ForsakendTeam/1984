8acaf59218f2408d0930c23d5e64045616e43378
ForsakendMJr <64882709+ForsakendMJr@users.noreply.github.com>
2021-11-16 03:12:29 +0000

    Final Clean up of V22

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+document.addEventListener("click", e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]")
+    currentDropdown.classList.toggle("active")
+  }
 
-    if (username === "Chamitha" && password === "1984-1") {
-        alert("You have successfully logged in.");
-        location.replace('https://forsakendteam.github.io/1984.github.io/hub.html');
-    } else if (username === "Samir" && password === "1984-2") {
-        alert("You have successfully logged in.");
-        location.replace('https://forsakendteam.github.io/1984.github.io/hub.html');
-    } else if (username === "Theo" && password === "1984-3") {
-        alert("You have successfully logged in.");
-        location.replace('https://forsakendteam.github.io/1984.github.io/hub.html');
-    } else if (username === "Jack" && password === "1984-4") {
-        alert("You have successfully logged in.");
-        location.replace('https://forsakendteam.github.io/1984.github.io/hub.html');
-    } 
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove("active")
+  })
 })
